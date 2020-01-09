892019066ba28294773da4f8017d963994e759cb
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-08 18:12:26 -0600

    Estructura inicial del proyecto

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="margen">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Correo</th>
+            <th>Enlace</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Jesus</td>
+            <td>jesudevelp@gmail.com</td>
+            <td>jesudevp.com</td>
+          </tr>
+          <tr>
+            <td>Ainat</td>
+            <td>ainat46@gmail.com</td>
+            <td>ainat97.com</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
