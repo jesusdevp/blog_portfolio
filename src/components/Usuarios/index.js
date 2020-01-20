08b4bb0dc4cb82161f299ecd3dc5039da6195b6f
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-19 18:29:58 -0600

    Se dividio la aplicacion en rutas  React-Router

---
+++ src/components/Usuarios/index.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+class Usuarios extends Component {
+  constructor() {
+    super();
+    this.state = {
+      usuarios: []
+    };
+  }
+
+  async componentDidMount() {
+    const respuesta = await axios.get(
+      " https://jsonplaceholder.typicode.com/users"
+    );
+
+    this.setState({
+      usuarios: respuesta.data
+    });
+  }
+
+  ponerFIlas = () =>
+    this.state.usuarios.map(usuario => (
+      <tr key={usuario.id}>
+        <td>{usuario.name}</td>
+        <td>{usuario.email}</td>
+        <td>{usuario.website}</td>
+      </tr>
+    ));
+
+  render() {
+    return (
+      <div className="margen">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+              <th>Enlace</th>
+            </tr>
+          </thead>
+          <tbody>{this.ponerFIlas()}</tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Usuarios;
