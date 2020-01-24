c52ebe407a28626e6641329d4ac3d37034d1ec56
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-24 10:30:13 -0600

    Parametros por URL

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Menu from "./Menu";
 import Usuarios from "./Usuarios";
+import Publicaciones from "./Publicaciones";
 
 const Tareas = () => <div>Tareas</div>;
 
@@ -10,7 +11,8 @@ const App = props => (
     <Menu />
     <div className="margen">
       <Route exact path="/" component={Usuarios} />
-      <Route exact path="/Tareas" component={Tareas} />
+      <Route exact path="/tareas" component={Tareas} />
+      <Route exact path="/publicaciones/:key" component={Publicaciones} />
     </div>
   </BrowserRouter>
 );
---
+++ src/components/Publicaciones/index.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Publicaciones extends Component {
+  render() {
+    return <div>{this.props.match.params.key}</div>;
+  }
+}
+
+export default Publicaciones;
