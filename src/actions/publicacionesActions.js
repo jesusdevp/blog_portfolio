8de38df4e847ab462a3755e678335ec68c9111bf
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-25 15:21:07 -0600

    inmutabilidad

--- src/actions/publicacionesActions.js
+++ src/actions/publicacionesActions.js
@@ -4,6 +4,9 @@ import {
   CARGANDO,
   ERROR
 } from "../types/publicacionesTypes";
+import * as usuariosTypes from "../types/usuariosTypes";
+
+const { TRAER_TODOS: USUARIOS_TRAER_TODOS } = usuariosTypes;
 
 export const traerTodos = () => async dispatch => {
   dispatch({
@@ -36,6 +39,18 @@ export const traerPorUsuario = key => async (dispatch, getState) => {
   );
 
   const publicaciones_actualizadas = [...publicaciones, respuesta.data];
+
+  const publicaciones_key = publicaciones_actualizadas.length - 1;
+  const usuarios_actualizados = [...usuarios];
+  usuarios_actualizados[key] = {
+    ...usuarios[key],
+    publicaciones_key
+  };
+
+  dispatch({
+    type: USUARIOS_TRAER_TODOS,
+    payload: usuarios_actualizados
+  });
 
   dispatch({
     type: TRAER_POR_USUARIO,
