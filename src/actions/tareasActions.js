f8db4f607507b304052d156e4596091ab5c1be2f
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-29 17:45:24 -0600

    Manejamos inputs con Reducers

--- src/actions/tareasActions.js
+++ src/actions/tareasActions.js
@@ -33,3 +33,17 @@ export const traerTodas = () => async dispatch => {
     });
   }
 };
+
+export const cambioUsuarioId = usuario_id => dispatch => {
+  dispatch({
+    type: "cambio_usuario_id",
+    payload: usuario_id
+  });
+};
+
+export const cambioTitulo = titulo => dispatch => {
+  dispatch({
+    type: "cambio_titulo",
+    payload: titulo
+  });
+};
--- src/components/Tareas/Guardar.js
+++ src/components/Tareas/Guardar.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import * as tareasActions from "../../actions/tareasActions";
 
 export class Guardar extends Component {
+  cambioUsuarioId = event => {
+    this.props.cambioUsuarioId(event.target.value);
+  };
+
+  cambioTitulo = event => {
+    this.props.cambioTitulo(event.target.value);
+  };
   render() {
     return (
       <div>
         <h1>Guardar Tarea</h1>
         Usuario id:
-        <input type="number" />
+        <input
+          type="number"
+          value={this.props.usuario_id}
+          onChange={this.cambioUsuarioId}
+        />
         <br />
         <br />
         Titulo:
-        <input />
+        <input value={this.props.titulo} onChange={this.cambioTitulo} />
         <br />
         <br />
         <button>Guardar</button>
@@ -19,4 +33,6 @@ export class Guardar extends Component {
   }
 }
 
-export default Guardar;
+const mapStateToProps = ({ tareasReducer }) => tareasReducer;
+
+export default connect(mapStateToProps, tareasActions)(Guardar);
