c072b7f2a41560c3c4778dc86be681ef6a759918
Jesus Ramirez <jesusdevelp@gmail.com>
2020-01-29 16:26:06 -0600

    Mapeamos objetos de tareas

--- src/components/Tareas/index.js
+++ src/components/Tareas/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import Spinner from "../General/Spinner";
+import Fatal from "../General/Fatal";
 
 import * as tareasActions from "../../actions/tareasActions";
 
@@ -8,9 +10,42 @@ export class Tareas extends Component {
     this.props.traerTodas();
   }
 
+  mostrarContenido = () => {
+    const { tareas, cargando, error } = this.props;
+
+    if (cargando) {
+      return <Spinner />;
+    }
+
+    if (error) {
+      return <Fatal mensaje={error} />;
+    }
+
+    return Object.keys(tareas).map(usu_id => (
+      <div key={usu_id}>
+        <h2>Usuario {usu_id}</h2>
+        <div className="contenedor_tareas">{this.ponerTareas(usu_id)}</div>
+      </div>
+    ));
+  };
+
+  ponerTareas = usu_id => {
+    const { tareas } = this.props;
+    const por_usuario = {
+      ...tareas[usu_id]
+    };
+
+    return Object.keys(por_usuario).map(tar_id => (
+      <div key={tar_id}>
+        <input type="checkbox" defaultChecked={por_usuario[tar_id].completed} />
+        {por_usuario[tar_id].title}
+      </div>
+    ));
+  };
+
   render() {
     console.log(this.props);
-    return <div>Tareas Saludar</div>;
+    return <div>{this.mostrarContenido()}</div>;
   }
 }
 
